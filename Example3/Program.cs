1130716a13581994b14b81c9ee105b5d02e7d41c
lVladimirl1 <vovon999999999@gmail.com>
2022-08-14 00:49:38 +0400

    Задача №6

---
+++ Example3/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
+//является ли число чётным (делится ли оно на два без остатка).
+
+
+Console.Clear();
+
+Console.WriteLine ("Введите число");
+int namber = Convert.ToInt32(Console.ReadLine());
+
+
+
+if (namber % 2 == 1)
+{
+    Console.WriteLine (namber);
+    Console.WriteLine ("Нечетное число");
+}
+else 
+{
+    Console.WriteLine (namber);
+    Console.WriteLine ("Четное число");
+}
+
+
+
+
+
+
+
+
+
